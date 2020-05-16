054a7507e6863d836ff5e9df05f6cd0dffca79cb
Murilo <marcosmeschial@gmail.com>
2020-05-15 22:54:39 -0300

    É agora!!!

--- views/login/theme/pagseguro/notificacao.php
+++ views/login/theme/pagseguro/notificacao.php
@@ -3,12 +3,6 @@
 require_once('configuracao.php');
 require_once('utils.php');
 
-$name = 'arquivo.txt';
-$text = "\n" . 'notificacao recebida em ' . date('Y-m-d') . "\n";
-$file = fopen($name, 'a');
-fwrite($file, $text);
-fclose($file);
-
 if(isset($_POST['notificationType']) && $_POST['notificationType'] == 'transaction'){
 
   $email = $PAGSEGURO_EMAIL;
@@ -30,24 +24,15 @@ if(isset($_POST['notificationType']) && $_POST['notificationType'] == 'transacti
 
   $status = $transaction->status;
   $idPedido = $transaction->reference;
-  $data = $transaction->lastEventDate;
-  $codigoTransacao = $transaction->code;
-  $metodoPagamentoType = $transaction->paymentMethod->type;
-  $metodoPagamentoCode = $transaction->paymentMethod->code;
 
-  $name = 'arquivo.txt';
-  // $text = var_export($_POST, true);
-  $text = "\n" . 'status = ' . $status . ' || id de referencia = ' . $idPedido . ' || data de transacao = ' . $data . ' || codigo de trasacao = ' . $codigoTransacao . ' || metodo de pagamento (tipo) = ' . $metodoPagamentoType . ' || metodo de pagamento (codigo) = ' . $metodoPagamentoCode;
-  $file = fopen($name, 'a');
-  fwrite($file, $text);
-  fclose($file);
 
-} else{
-  $name = 'arquivo.txt';
-  $text = 'nao foi possivel gravar no arquivo.txt';
-  $file = fopen($name, 'a');
-  fwrite($file, $text);
-  fclose($file);
+  if (!empty($xml->reference)){
+    $venda = (new \Source\Models\ContrataRota())->findById($idPedido);
+    $venda->status = $status;
+    $venda->save();
+  }
+
+
 }
 
 ?>
