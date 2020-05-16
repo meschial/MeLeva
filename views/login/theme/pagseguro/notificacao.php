c5734372ea3607467eb5f5f322628803a2e8a941
Murilo <marcosmeschial@gmail.com>
2020-05-15 22:09:43 -0300

    É agora!!!

--- views/login/theme/pagseguro/notificacao.php
+++ views/login/theme/pagseguro/notificacao.php
@@ -1,11 +1,10 @@
 <?php
-require_once('configuracao.php');
 require_once('utils.php');
 
 if(isset($_POST['notificationType']) && $_POST['notificationType'] == 'transaction'){
 
-  $email = $PAGSEGURO_EMAIL;
-  $token = $PAGSEGURO_TOKEN;
+  $email = 'formatacaoumuarama@gmail.com';
+  $token = '1045640749614566A06AA642AD42B89E';
 
   $url = 'https://ws.sandbox.pagseguro.uol.com.br/v2/transactions/notifications/' . $_POST['notificationCode'] . '?email=' . $email . '&token=' . $token;
 
@@ -14,11 +13,6 @@ if(isset($_POST['notificationType']) && $_POST['notificationType'] == 'transacti
   curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
   $transaction= curl_exec($curl);
   curl_close($curl);
-
-  if($transaction == 'Unauthorized'){
-    print_r("nao autorizado");
-    exit;
-  }
 
   $xml = json_decode(json_encode(simplexml_load_string($transaction)));
 
