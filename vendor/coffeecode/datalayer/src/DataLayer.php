08e427ed345bf957b5b3071751b34e3c8d369351
Murilo <marcosmeschial@gmail.com>
2020-05-21 13:18:33 -0300

    DataLayer

--- vendor/coffeecode/datalayer/src/DataLayer.php
+++ vendor/coffeecode/datalayer/src/DataLayer.php
@@ -219,7 +219,7 @@ abstract class DataLayer
      * @param bool $all
      * @return array|mixed|null
      */
-    public function fetch(bool $all = false): DataLayer
+    public function fetch(bool $all = false)
     {
         try {
             $stmt = Connect::getInstance()->prepare($this->statement . $this->group . $this->order . $this->limit . $this->offset);
@@ -251,7 +251,7 @@ abstract class DataLayer
     }
 
     /**
-     * @return bool|DataLayer
+     * @return bool
      */
     public function save(): bool
     {
