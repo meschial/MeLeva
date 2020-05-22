1a58594a146425c37822b403d0b5673e309de811
Murilo <marcosmeschial@gmail.com>
2020-05-22 20:09:39 -0300

    Cencelar pagamento

--- views/login/admin/pagamentos/cancelarpagamento.php
+++ views/login/admin/pagamentos/cancelarpagamento.php
@@ -4,7 +4,6 @@ $email = 'formatacaoumuarama@gmail.com';
 $token = '1045640749614566A06AA642AD42B89E';
 
 $code = $_POST['id'];
-var_dump($code);
   $Url="https://ws.sandbox.pagseguro.uol.com.br/v2/transactions/cancels?email=".$email."&token=".$token."&transactionCode={$code}";
 
   $Curl=curl_init($Url);
