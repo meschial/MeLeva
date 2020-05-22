93e4ad32bbc776750fefd944cea31dc1802d65af
Murilo <marcosmeschial@gmail.com>
2020-05-22 20:14:37 -0300

    Cencelar pagamento

--- views/login/admin/pagamentos/pagamentosdetalhe.php
+++ views/login/admin/pagamentos/pagamentosdetalhe.php
@@ -163,6 +163,7 @@ foreach ($xml->shipping as $adress) {
                     success: function(retorna){
                         console.log("Sucesso " + JSON.stringify(retorna));
                         swal('Atenção', 'Pagamento cancelado!', 'success');
+                        location.reload();
                     },
                     error: function(retorna){
                         console.log("Erro" + JSON.stringify(retorna));
