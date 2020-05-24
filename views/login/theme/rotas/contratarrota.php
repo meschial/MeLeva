7c24a0097cc9ab318a4ba7d3070f8a4632b5c4e5
Murilo <marcosmeschial@gmail.com>
2020-05-24 13:19:19 -0300

    Arrumando contratarrota.php

--- views/login/theme/rotas/contratarrota.php
+++ views/login/theme/rotas/contratarrota.php
@@ -5,29 +5,35 @@
         <div class="single-content1">
             <div class="single-job mb-4 justify-content-between">
                 <div class="job-text">
-                    <h1 style="color: #6c757d; text-align: center">Nova Rota</h1>
+                    <h1 style="color: #6c757d; text-align: center">Contratar Rota</h1>
                     <ul class="mt-4">
                         <form class="auth_form" action="<?= $router->route("app.contrata") ?>" method="post" enctype="multipart/form-data">
 
                             <div class="login_form_callback">
                               <?= flash(); ?>
                             </div>
-                            <input type="text" readonly value="<?= $rotas->id ?>" class="form-control" name="id_rota">
-                            <input type="text" readonly value="<?= $rotas->login_id ?>" class="form-control" name="id_mot">
+                            <h3><b>Detalhes da Rota</b></h3>
+                            <h4>Cidade Origem: <?= $rotas->cidade_inicio ?> | Cidade Destino: <?= $rotas->cidade_fim?></h4>
+                            <h5>CEP Origem: <?= $rotas->cep_inicio ?> | CEP Destino: <?= $rotas->cep_fim?> | ValorR$: <?= number_format($rotas->valor, 2, ',', '') ?></h5><br>
+                            <input type="hidden" readonly value="<?= $rotas->id ?>" class="form-control" name="id_rota">
+                            <input type="hidden" readonly value="<?= $rotas->login_id ?>" class="form-control" name="id_mot">
 
                             <div class="form-row">
-                                <div class="form-group col-md-4">
+                                <div class="form-group col-md-12">
                                     <label>Nome do Produto:</label>
                                     <input type="text" class="form-control" name="nome">
-
                                 </div>
-                                <div class="form-group col-md-4">
-                                    <label>Descrição:</label>
-                                    <input type="text" class="form-control" name="descricao">
+
+                            </div>
+                            <div class="form-row">
+
+                                <div class="form-group col-md-12">
+                                    <label>Descrição do produto:</label>
+                                    <textarea type="text" class="form-control" name="descricao" rows="5" maxlength="200"></textarea>
                                 </div>
                             </div>
                             <div>
-                                <button type="submit" class="btn btn-primary btn-lg btn-block active">Cadastrar</button>
+                                <button type="submit" class="btn btn-primary btn-lg btn-block active">Contratar</button>
                             </div>
                         </form>
                     </ul>
