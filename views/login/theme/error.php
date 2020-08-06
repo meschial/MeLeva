290c66d46cf4e4463876df817624aee288ffce20
Murilo <marcosmeschial@gmail.com>
2020-08-05 23:27:28 -0300

    Subindo todos os dados

--- views/login/theme/error.php
+++ views/login/theme/error.php
@@ -1,6 +1,6 @@
 <?php $v->layout("theme/theme"); ?>
 
 <div class="container">
-    <h1 class="text-center">Ooops. Tivemos um erro!</h1><br>
+    <h1 class="text-center">Ooops. Tivemos um erro!</h1></br>
     <img src="https://websiteshop.com.br/wp-content/uploads/2019/06/pagina-de-erro-site-1160x665.jpg">
 </div>
